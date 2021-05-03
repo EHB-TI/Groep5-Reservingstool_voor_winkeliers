fc9d2166b73db7d7239fc78386af80ff7dd76c1d
RyanDV38 <ryan.de.vogel@ehb.be>
2021-05-03 17:10:49 +0200

    Added default layout

--- src/main/java/ehb/group5/app/UI/login/LoginView.java
+++ src/main/java/ehb/group5/app/UI/login/LoginView.java
@@ -1,16 +1,20 @@
 package ehb.group5.app.UI.login;
 
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import jdk.javadoc.internal.doclets.formats.html.Navigation;
 
 @Route("login")
 @PageTitle("Aanmelden als winkelier")
@@ -19,12 +23,36 @@ public class LoginView extends VerticalLayout {
 
 
     public LoginView() {
-        // Use TextField for standard text input
-       add(new H1("Hello world"));
 
-       Button button = new Button("click me", click -> {
-           add(new Span("you clicked"));
-       });
+       add(new H1("Kies uw plan"));
+
+       Div div = new Div();
+       Section left = new Section();
+       Section middel = new Section();
+       Section right = new Section();
+       H3 titel1 = new H3("1 maand");
+       H3 titel2 = new H3("3 maanden");
+       H3 titel3 = new H3("12 maanden");
+
+
+       left.add(titel1);
+       middel.add(titel2);
+       right.add(titel3);
+
+       for(int i=0; i<3;i++) {
+           Paragraph p = new Paragraph("Lorem Ipsum");
+           Paragraph p1 = new Paragraph("Lorem Ipsum");
+           Paragraph p2 = new Paragraph("Lorem Ipsum");
+           left.add(p);
+           middel.add(p1);
+           right.add(p2);
+       }
+
+       div.add(left);
+       div.add(middel);
+       div.add(right);
+       add(div);
+
     }
 
 }
