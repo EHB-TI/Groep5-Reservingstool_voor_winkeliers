dc9c7226b2eaf074eb7c42773dc8c5fde4b6565e
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-03 21:38:19 +0200

    Configurations file generation

--- src/main/java/ehb/group5/app/backend/config/SQLCredentials.java
+++ src/main/java/ehb/group5/app/backend/config/SQLCredentials.java
@@ -1,20 +1,19 @@
 package ehb.group5.app.backend.config;
 
 import com.zaxxer.hikari.HikariConfig;
+import lombok.Data;
 import lombok.val;
 
+@Data
 public class SQLCredentials {
-    private String host = "dt5.ehb.be";
-    private String database = "2021PROGPROJGR5";
-    private String username = "2021PROGPROJGR5";
-    private String password = "8uGuEtMV";
+    private String host, database, username, password = "";
     private int poolSize = 5;
     private int port = 3306;
 
     public HikariConfig toHikari(){
         val config = new HikariConfig();
 
-        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
         config.setJdbcUrl("jdbc:mysql://" + host + ":" + port + "/" + database);
         config.setUsername(username);
         config.setPassword(password);
