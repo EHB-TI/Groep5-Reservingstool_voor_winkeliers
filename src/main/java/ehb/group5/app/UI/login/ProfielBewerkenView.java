7ec2aa479803ad13f05562f998e3b3c7c7099c7f
Your Name <you@example.com>
2021-05-03 17:50:32 +0200

    Profiel disign en Support services aan het maken

--- src/main/java/ehb/group5/app/UI/login/ProfielBewerkenView.java
+++ src/main/java/ehb/group5/app/UI/login/ProfielBewerkenView.java
@@ -2,7 +2,10 @@ package ehb.group5.app.UI.login;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -10,23 +13,42 @@ import com.vaadin.flow.router.Route;
 @PageTitle("ProfielBewerken")
 @CssImport("./styles/ProfielBewerken.css")
 
+
+
+
 public class ProfielBewerkenView extends VerticalLayout{
 
     public ProfielBewerkenView(){
-        Button button = new Button("Nieuwe Email");
+
+        Div div = new Div();
+
+        add(new H1("Profiel bewerken"));
+
+        TextField labelField = new TextField();
+        labelField.setLabel("Nieuwe Email");
+
+        add(labelField);
+
+        TextField labelField2 = new TextField();
+        labelField2.setLabel("Nieuwe Voornaam");
+
+        add(labelField2);
+
+        TextField labelField3 = new TextField();
+        labelField3.setLabel("Nieuwe Achternaam");
+
+        add(labelField3);
+
+        TextField labelField4 = new TextField();
+        labelField4.setLabel("Nieuwe Wachtwoord");
+
+        add(labelField4);
+
+        Button button = new Button("Save");
         add(button);
 
-        Button button2 = new Button("Nieuwe Voornaam");
-        add(button2);
-
-        Button button3 = new Button("Nieuwe Achternaam");
-        add(button3);
-
-        Button button4 = new Button("Nieuwe Wachtwoord");
-        add(button4);
-
-        Button button5 = new Button("Save");
-        add(button5);
 
     }
+
+
 }
--- src/main/java/ehb/group5/app/UI/login/SupportView.java
+++ src/main/java/ehb/group5/app/UI/login/SupportView.java
@@ -1,21 +1,38 @@
 package ehb.group5.app.UI.login;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 
 
-@Route("")
+@Route("support")
+@PageTitle("Support services")
+@CssImport("./styles/support.css")
 
 public class SupportView extends VerticalLayout {
 
     public SupportView(){
 
-        Button button = new Button("click me");
+        add(new H1("Support services"));
+
+        Button button = new Button("Ik heb een probleem met de facturatie");
         add(button);
+
+        Button button2 = new Button("Ik heb een probleem met de betaling");
+        add(button2);
+
+        Button button3 = new Button("Ik heb een probleem met het bewerken van de informatie ");
+        add(button3);
+
+        Button button4 = new Button("Ik heb een probleem met de agenda");
+        add(button4);
+
+        Button button5 = new Button("Andere probleem");
+        add(button5);
     }
-
-
 
 }
