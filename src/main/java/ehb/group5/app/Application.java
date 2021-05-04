bd1845eb1a310cccfc364bc5cccd381fdfc72d41
RyanDV38 <ryan.de.vogel@ehb.be>
2021-05-04 18:46:47 +0200

    added navigation bar to page.

--- src/main/java/ehb/group5/app/Application.java
+++ src/main/java/ehb/group5/app/Application.java
@@ -13,6 +13,7 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
     }
 
 }
