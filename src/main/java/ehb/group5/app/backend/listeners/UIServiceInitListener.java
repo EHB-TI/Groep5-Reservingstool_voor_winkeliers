4544e1e4b2eb20aaeeb7c964c43c141bf918e09e
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-05 18:50:11 +0200

    Page not found initialized

--- src/main/java/ehb/group5/app/backend/listeners/UIServiceInitListener.java
+++ src/main/java/ehb/group5/app/backend/listeners/UIServiceInitListener.java
@@ -2,14 +2,17 @@ package ehb.group5.app.backend.listeners;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.RouteNotFoundError;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.server.VaadinSession;
 import ehb.group5.app.UI.views.LoginView;
+import ehb.group5.app.UI.views.NoteFoundView;
 import org.springframework.stereotype.Component;
 
 /**
  * Source: https://vaadin.com/docs/v14/flow/tutorials/in-depth-course/login-and-authentication
+ *
  * @author Vaadin
  */
 @Component
@@ -24,9 +27,12 @@ public class UIServiceInitListener implements VaadinServiceInitListener {
     }
 
     private void authenticateNavigation(BeforeEnterEvent event) {
-        if (!LoginView.class.equals(event.getNavigationTarget())
+        System.out.println(event.getNavigationTarget());
+        if (!event.getNavigationTarget().equals(LoginView.class)
                 && VaadinSession.getCurrent().getAttribute("company") == null) {
             event.forwardTo(LoginView.class);
+        } else if (event.getNavigationTarget().equals(RouteNotFoundError.class)) {
+            event.rerouteTo(NoteFoundView.class);
         }
     }
 }
