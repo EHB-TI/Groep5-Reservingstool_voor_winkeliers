87cb5c1e3e81c22e4f5c1cd89975415b21670883
Faille Arnaud <arnaudar.faille@gmail.com>
2021-05-06 14:27:24 +0200

    Making LoginView

--- src/main/java/ehb/group5/app/UI/views/NoteFoundView.java
+++ src/main/java/ehb/group5/app/UI/views/NoteFoundView.java
@@ -3,6 +3,7 @@ package ehb.group5.app.UI.views;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
