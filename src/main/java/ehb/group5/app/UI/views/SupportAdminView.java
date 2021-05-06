94c110f925edb8d8d2ab0f8b003bbebbc415746d
jasondevedeleer <jason.devedeleer@student.ehb.be>
2021-05-06 15:28:52 +0200

    style nog af te werken

--- src/main/java/ehb/group5/app/UI/views/SupportAdminView.java
+++ src/main/java/ehb/group5/app/UI/views/SupportAdminView.java
@@ -1,6 +1,7 @@
 package ehb.group5.app.UI.views;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -9,18 +10,44 @@ import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.shared.Registration;
-import ehb.group5.app.UI.layouts.CommonLayout;
 
 
 import java.awt.*;
 
-@Route("support/admin")
+@Route("Support/Admin")
 @PageTitle("Support/Admin")
 @CssImport("./styles/style.css")
-public class SupportAdminView extends CommonLayout {
+
+public class SupportAdminView extends HorizontalLayout{
 
     public SupportAdminView(){
-        getContainer().add(new H1("Ticket"));
+
+
+        VerticalLayout v2 = new VerticalLayout();
+        add(v2);
+
+        v2.add(new H1("Ticket"));
+
+        Button b1 = new Button("Ticket#1");
+        v2.add(b1);
+
+        Button b2 = new Button("Ticket#2");
+        v2.add(b2);
+
+        Button b3 = new Button("Ticket#3");
+        v2.add(b3);
+
+        Button b4 = new Button("Ticket#4");
+        v2.add(b4);
+
+        VerticalLayout v1 = new VerticalLayout();
+        add(v1);
+
+        v1.add(new H3("klantennummer"));
+        v1.add(new H3("naam"));
+        v1.add(new H3("beschrijving probleem"));
+
+
     }
 
 }
