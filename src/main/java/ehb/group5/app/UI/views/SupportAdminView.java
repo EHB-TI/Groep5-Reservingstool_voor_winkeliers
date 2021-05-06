3a7fa6f9db561e666056acb0866fa04197c6aca3
jasondevedeleer <jason.devedeleer@student.ehb.be>
2021-05-06 22:19:22 +0200

    SupportAdminView klaar

--- src/main/java/ehb/group5/app/UI/views/SupportAdminView.java
+++ src/main/java/ehb/group5/app/UI/views/SupportAdminView.java
@@ -10,42 +10,51 @@ import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.shared.Registration;
+import ehb.group5.app.UI.layouts.CommonLayout;
 
 
 import java.awt.*;
 
 @Route("Support/Admin")
 @PageTitle("Support/Admin")
-@CssImport("./styles/style.css")
+@CssImport("./styles/SupportAdmin.css")
 
-public class SupportAdminView extends HorizontalLayout{
+public class SupportAdminView extends CommonLayout {
 
     public SupportAdminView(){
 
+        VerticalLayout v1 = new VerticalLayout();
+        add(v1);
+        v1.setId("v2");
+
+        v1.add(new H1("Ticket"));
+
+
+        Button b1 = new Button("Ticket#1");
+        v1.add(b1);
+        b1.setId("button");
+
+        Button b2 = new Button("Ticket#2");
+        v1.add(b2);
+        b2.setId("button");
+
+        Button b3 = new Button("Ticket#3");
+        v1.add(b3);
+        b3.setId("button");
+
+        Button b4 = new Button("Ticket#4");
+        v1.add(b4);
+        b4.setId("button");
 
         VerticalLayout v2 = new VerticalLayout();
         add(v2);
+        v2.setId("v1");
 
-        v2.add(new H1("Ticket"));
+        v2.add(new H3("klantennummer"));
+        v2.add(new H3("naam"));
+        v2.add(new H3("beschrijving probleem"));
 
-        Button b1 = new Button("Ticket#1");
-        v2.add(b1);
-
-        Button b2 = new Button("Ticket#2");
-        v2.add(b2);
-
-        Button b3 = new Button("Ticket#3");
-        v2.add(b3);
-
-        Button b4 = new Button("Ticket#4");
-        v2.add(b4);
-
-        VerticalLayout v1 = new VerticalLayout();
-        add(v1);
-
-        v1.add(new H3("klantennummer"));
-        v1.add(new H3("naam"));
-        v1.add(new H3("beschrijving probleem"));
+        getContainer().add(v1,v2);
 
 
     }
