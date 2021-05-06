6af5d0567da9ce105297fc29975b7be343b00841
jasondevedeleer <jason.devedeleer@student.ehb.be>
2021-05-06 22:46:30 +0200

    SupportAdminView klaar

--- src/main/java/ehb/group5/app/UI/views/ProfielView.java
+++ src/main/java/ehb/group5/app/UI/views/ProfielView.java
@@ -7,17 +7,20 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import ehb.group5.app.UI.layouts.CommonLayout;
 
 @Route("Profiel")
 @PageTitle("Profiel")
-@CssImport("./styles/style.css")
-public class ProfielView extends HorizontalLayout {
+@CssImport("./styles/profiel.css")
+public class ProfielView extends CommonLayout {
 
 
         public ProfielView() {
 
             VerticalLayout v1 = new VerticalLayout();
             add(v1);
+            v1.setId("v10");
+
             v1.add(new H1("profiel"));
             v1.add(new H3("email:"));
             v1.add(new H3("voornaam:"));
@@ -29,14 +32,21 @@ public class ProfielView extends HorizontalLayout {
 
             VerticalLayout v2 = new VerticalLayout();
             add(v2);
+            v2.setId("v20");
+
             Button b1 = new Button("profiel bewerken");
             v2.add(b1);
+            b1.setId("button1");
 
             Button b2 = new Button("nieuwe medewerker");
             v2.add(b2);
+            b2.setId("button1");
 
             Button b3 = new Button("verwijder medewerker");
             v2.add(b3);
+            b3.setId("button1");
+
+            getContainer().add(v1,v2);
 
         }
 
