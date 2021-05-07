8265d61772618b317d90bf4a1daca3c8d7180e0c
Your Name <you@example.com>
2021-05-07 11:30:10 +0200

    ProfielBewerkenView linken mete de database en updaten tegenover nieuwe input.

--- src/main/java/ehb/group5/app/UI/views/ProfielBewerkenView.java
+++ src/main/java/ehb/group5/app/UI/views/ProfielBewerkenView.java
@@ -1,5 +1,6 @@
 package ehb.group5.app.UI.views;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
@@ -8,9 +9,12 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import ehb.group5.app.UI.layouts.CommonLayout;
+import ehb.group5.app.backend.data.DatabaseService;
+import ehb.group5.app.backend.data.table.CompanyEntity;
 
 @Route("profiel/edit")
 @PageTitle("ProfielBewerken")
@@ -25,6 +29,9 @@ public class ProfielBewerkenView extends CommonLayout {
 
     public ProfielBewerkenView(){
 
+
+        CompanyEntity company = (CompanyEntity) VaadinSession.getCurrent().getAttribute("company");
+
         Div div = new Div();
 
         //Titel aanmaken
@@ -35,13 +42,14 @@ public class ProfielBewerkenView extends CommonLayout {
 
         //Label initialiseren
         labelField.setLabel("Nieuwe Email");
+        labelField.setValue(company.getEmail());
 
         //De maximum limiet van letters implementeren
         labelField.setMaxLength(30);
 
         div.add(labelField);
 
-        TextField labelField2 = new TextField();
+        /*TextField labelField2 = new TextField();
         labelField2.setLabel("Nieuwe Voornaam");
         labelField2.setMaxLength(20);
 
@@ -51,7 +59,7 @@ public class ProfielBewerkenView extends CommonLayout {
         labelField3.setLabel("Nieuwe Achternaam");
         labelField3.setMaxLength(20);
 
-        div.add(labelField3);
+        div.add(labelField3);*/
 
         TextField labelField4 = new TextField();
         labelField4.setLabel("Nieuwe Wachtwoord");
@@ -62,13 +70,16 @@ public class ProfielBewerkenView extends CommonLayout {
         //knop aanmaken
         Button button = new Button("Save");
         div.add(button);
+        button.addClickListener(buttonClickEvent ->{
+            company.setEmail(labelField.getValue());
+            company.setPassword(labelField4.getValue());
+            DatabaseService.getCompaniesStore().update(company);
+            UI.getCurrent().getPage().setLocation("profiel");
+        });
 
         div.addClassName("centered-content");
         getContainer().add(div);
 
-
-
     }
-
 
 }
