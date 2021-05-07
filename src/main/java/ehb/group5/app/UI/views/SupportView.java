37421c55f499540696067cf6097ef9dd7b739c7e
Zakaria Lamsakam <zakaria.lamsakam@student.ehb.be>
2021-05-07 15:06:16 +0200

    SupportView linken met de database, stylen van frontend en updaten .

--- src/main/java/ehb/group5/app/UI/views/SupportView.java
+++ src/main/java/ehb/group5/app/UI/views/SupportView.java
@@ -37,10 +37,11 @@ public class SupportView extends CommonLayout {
 
         //notificatie tonen wanneer er op een button wordt gedrukt.
         Dialog dialog = new Dialog();
-        dialog.add(new Text("Bel deze telefoon voor support : 0124592598"));
+        dialog.add(new Text("Uw factuur bereikt zijn bestemming niet door onjuiste gegevens. Vraag vooraf alle gegevens op, begin met het e-mailadres van de verantwoordelijke voor de betaling van de facturen.\"" +
+                ""+" Andere problemen bel dan deze telefoonnummer voor support: 0124592598"));
 
-        dialog.setWidth("400px");
-        dialog.setHeight("150px");
+        dialog.setWidth("600px");
+        dialog.setHeight("250px");
 
         button.addClickListener(event -> dialog.open());
 
@@ -49,12 +50,13 @@ public class SupportView extends CommonLayout {
         div.add(button2);
 
         Dialog dialog2 = new Dialog();
-        dialog2.add(new Text("Bel deze telefoon voor support: 0124392594"));
+        dialog2.add(new Text("U kan dit oplossen door één of meerdere minder dringende overschrijvingen uit te vinken.\n" +
+                " Vervolgens tekent u enkel de aangevinkte verrichtingen. "+" Andere problemen bel dan deze telefoonnummer voor support: 0124392594"));
 
-        dialog.setWidth("400px");
-        dialog.setHeight("150px");
+        dialog.setWidth("600px");
+        dialog.setHeight("250px");
 
-        button2.addClickListener(event -> dialog.open());
+        button2.addClickListener(event -> dialog2.open());
 
         Button button3 = new Button("Ik heb een probleem met het bewerken van de informatie ");
         div.add(button3);
@@ -62,24 +64,25 @@ public class SupportView extends CommonLayout {
 
 
         Dialog dialog3 = new Dialog();
-        dialog3.add(new Text("Bel deze telefoon voor support: 0184592599"));
+        dialog3.add(new Text("BIC code (Bank Identification Code) is een unieke code die één welbepaalde bank identificeert en bestaat meestal uit 8 of 11 tekens."+" Andere problemen bel dan deze telefoonnummer voor support: 0184592599"));
 
-        dialog.setWidth("400px");
-        dialog.setHeight("150px");
+        dialog.setWidth("600px");
+        dialog.setHeight("250px");
 
-        button3.addClickListener(event -> dialog.open());
+        button3.addClickListener(event -> dialog3.open());
 
         Button button4 = new Button("Ik heb een probleem met de agenda");
         div.add(button4);
 
 
         Dialog dialog4 = new Dialog();
-        dialog4.add(new Text("Bel deze telefoon voor support: 0124692298"));
+        dialog4.add(new Text("1. Zorg ervoor dat je verbinding met internet hebt, controleer of de agenda zichtbaar is, zorg dat nieuwe afspraken worden toegevoegd aan je Google Agenda."
+                +" Andere problemen bel dan deze telefoonnummer voor support: 0124692298"));
 
-        dialog.setWidth("400px");
-        dialog.setHeight("150px");
+        dialog.setWidth("600px");
+        dialog.setHeight("250px");
 
-        button4.addClickListener(event -> dialog.open());
+        button4.addClickListener(event -> dialog4.open());
 
         Button button5 = new Button("Andere probleem");
         div.add(button5);
@@ -87,12 +90,12 @@ public class SupportView extends CommonLayout {
 
 
         Dialog dialog5 = new Dialog();
-        dialog5.add(new Text("Bel deze telefoon voor support: 0174992568"));
+        dialog5.add(new Text("Andere problemen bel dan deze telefoonnummer voor support: 0174992568"));
 
-        dialog.setWidth("400px");
-        dialog.setHeight("150px");
+        dialog.setWidth("600px");
+        dialog.setHeight("250px");
 
-        button5.addClickListener(event -> dialog.open());
+        button5.addClickListener(event -> dialog5.open());
 
         div.addClassName("centered-content");
         getContainer().add(div);
